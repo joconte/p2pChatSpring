6077060a78798375d529601b290822ac487a0fa0
joconte <etud@contejonathan.com>
2020-01-14 20:07:48 +0100

    remove unused import

--- src/main/java/fr/epsi/jconte/p2pchat/controller/front/IConversationController.java
+++ src/main/java/fr/epsi/jconte/p2pchat/controller/front/IConversationController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
 import java.util.List;
 
 @RequestMapping("/chat/conversation")
